fbf74c365e2b270d1e9fb6da7369e5836308996d
Filip Pyszny <filip.pyszny@gmail.com>
2022-09-24 12:48:34 +0200

    [NEW] Added sleep-wake tests and toggle output wioth jitter; cleaned code a bit

--- main.c
+++ main.c
@@ -2,12 +2,15 @@
 #include <stdint.h>
 #include "pico/stdlib.h"
 #include "hardware/structs/xip_ctrl.h"
-#include "pico/stdlib.h"
+#include "pico/sleep.h"
 #include "hardware/pwm.h"
 #include "hardware/irq.h"
 #include "hardware/gpio.h"
 #include "hardware/structs/iobank0.h"
 #include "hardware/structs/sio.h"
+#include "hardware/rosc.h"
+#include "hardware/structs/scb.h"
+#include "hardware/clocks.h"
 
 #define dXIPdisabled    ( 1 )
 
@@ -15,31 +18,57 @@
 #define dIRQSourceCallback  ( 0 )
 #define dIRQSourceRaw       ( 0 )
 
+/* Alternative IRQ pin for pin-impact test or sleep-wakeup tests */
+#define dUseAltIRQPin       ( 0 )
+
+/* Enable of other tests done with IRQ system, change to "1" to enable */
+#define dEnableSleepWakeTest          ( 0 ) 
+#define dEnableOutputToggleWithJitter ( 0 ) 
+
 #define dGPIO_IRQ   ( 0 )
 #define dGPIO_OUT1  ( 2 )
 #define dGPIO_OUT2  ( 3 )
 #define dGPIO_PWM   ( 4 )
+#define dGPIO_altIRQ ( 22 )
 
 void GPIO_IRQHandlerFunc( uint gpio, uint32_t events );
 void GPIO_ExclusiveIRQHandlerFunc();
 void GPIO_Initialize();
 void PWM_Initialize();
-
-bool bSetting = false;
+void recover_from_sleep(uint scb_orig, uint clock0_orig, uint clock1_orig);
+void ToggleWithJitter();
 
 int main()
 {
-    #if dXIPdisabled
-    /* Disable the XIP for no-XIP variants */
-    xip_ctrl_hw->ctrl &= ~(1 << 0);
+  #if dXIPdisabled
+  /* Disable the XIP for no-XIP variants */
+  xip_ctrl_hw->ctrl &= ~(1 << 0);
+  #endif
+
+  GPIO_Initialize();
+  PWM_Initialize();
+
+  #if dEnableSleepWakeTest
+  uint scb_orig = scb_hw->scr;
+  uint clock0_orig = clocks_hw->sleep_en0;
+  uint clock1_orig = clocks_hw->sleep_en1;
+  #endif
+
+  while(1)
+  {
+
+    #if dEnableSleepWakeTest
+    sleep_ms(3000);
+    sleep_run_from_xosc();
+    sleep_goto_dormant_until_edge_high(dGPIO_altIRQ);
+
+    recover_from_sleep(scb_orig, clock0_orig, clock1_orig);  
     #endif
 
-    GPIO_Initialize();
-    PWM_Initialize();
-
-    while(1)
-    {
-    }
+    #if dEnableOutputToggleWithJitter
+    ToggleWithJitter();
+    #endif
+  }
 
     return 0;
 }
@@ -60,11 +89,14 @@ void GPIO_Initialize()
     gpio_set_dir(dGPIO_IRQ, false);
     gpio_set_pulls(dGPIO_IRQ, true, false);
 
+    gpio_init(dGPIO_altIRQ);
+    gpio_set_dir(dGPIO_altIRQ, false);
+    // added pull down for dormant wake
+    gpio_set_pulls(dGPIO_altIRQ, false, true);
+
     gpio_init(dGPIO_PWM);
     gpio_set_dir(dGPIO_PWM, true);
     gpio_set_function(dGPIO_PWM, GPIO_FUNC_PWM);
-
-    // gpio_set_dormant_irq_enabled
 
     /* 
      * Interrupt source setting 
@@ -74,7 +106,13 @@ void GPIO_Initialize()
     #if dIRQSourceExclusive
     /* Option 1: exclusive handler*/
     irq_set_exclusive_handler(IO_IRQ_BANK0, GPIO_ExclusiveIRQHandlerFunc);
+
+    #if dUseAltIRQPin
+    gpio_set_irq_enabled(dGPIO_altIRQ, 0x4, true);
+    #else
     gpio_set_irq_enabled(dGPIO_IRQ, 0x4, true);
+    #endif
+
     irq_set_enabled(IO_IRQ_BANK0, true);
     #endif
 
@@ -121,10 +159,71 @@ void GPIO_IRQHandlerFunc( uint gpio, uint32_t events )
     padsbank0_hw->io[dGPIO_OUT2] ^= PADS_BANK0_GPIO0_OD_BITS;
 }
 
-void GPIO_ExclusiveIRQHandlerFunc()
+void __not_in_flash_func(GPIO_ExclusiveIRQHandlerFunc)(void)
 {
     padsbank0_hw->io[dGPIO_OUT1] ^= PADS_BANK0_GPIO0_OD_BITS;
     padsbank0_hw->io[dGPIO_OUT2] ^= PADS_BANK0_GPIO0_OD_BITS;
   /* Clear IRQ - if not this will be caled infinitely */
+  #if dUseAltIRQPin
+  iobank0_hw->intr[dGPIO_altIRQ / 8] = 0xF << 4 * (dGPIO_altIRQ % 8);
+  #else
   iobank0_hw->intr[dGPIO_IRQ / 8] = 0xF << 4 * (dGPIO_IRQ % 8);
+  #endif
+
+}
+
+void recover_from_sleep(uint scb_orig, uint clock0_orig, uint clock1_orig)
+{
+  /* 
+   * Section below taken from: https://ghubcoder.github.io/posts/awaking-the-pico/ 
+   * Thank you, @ghubcoder for your investigation and article, lot of work and it really does the job!
+   */
+    //Re-enable ring Oscillator control
+    rosc_write(&rosc_hw->ctrl, ROSC_CTRL_ENABLE_BITS);
+
+    //reset procs back to default
+    scb_hw->scr = scb_orig;
+    clocks_hw->sleep_en0 = clock0_orig;
+    clocks_hw->sleep_en1 = clock1_orig;
+
+    //reset clocks
+    clocks_init();
+    stdio_init_all();
+
+    return;
+}
+
+/* Function used for other tests of IRQ response time - changing pin state with little "jitter" */
+void ToggleWithJitter()
+{
+  static uint32_t u32Counter = 0;
+  static uint16_t u16Shift = 0;
+  bool bSetting = false;
+
+  gpio_init(dGPIO_OUT1);
+  gpio_set_dir(dGPIO_OUT1, true);
+  gpio_put(dGPIO_OUT1, true);
+  gpio_set_pulls(dGPIO_OUT1, false, true);
+
+  gpio_init(25);
+  gpio_set_dir(25, true);
+  gpio_put(25, false);
+
+  while(1)
+  {
+    u32Counter++;
+
+    if( (u32Counter + u16Shift) > 10000000)
+    { 
+      u32Counter = 0;
+      u16Shift++;
+      if(u16Shift > 20000)
+      {
+        u16Shift=0;
+      }
+      bSetting = gpio_get( dGPIO_OUT1 );
+      gpio_put( dGPIO_OUT1, !bSetting );
+      gpio_put( 25, !bSetting );
+    }
+  }
 }
